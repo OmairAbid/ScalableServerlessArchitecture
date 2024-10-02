33f398d57b6761d6ddc1eaceb1ce4df545f06701
Umair <umair.abid96@gmail.com>
2024-10-02 16:34:13 +0400

    feat: add loadbalancing

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -1,10 +1,33 @@
 
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("authenticated", policy =>
+        policy.RequireAuthenticatedUser());
+});
+
+builder.Services.AddRateLimiter(rateLimiterOptions =>
+{
+    rateLimiterOptions.AddFixedWindowLimiter("fixed", options =>
+    {
+        options.Window = TimeSpan.FromSeconds(10);
+        options.PermitLimit = 5;
+    });
+});
+
 var app = builder.Build();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.UseRateLimiter();
 
 app.MapReverseProxy();
 
